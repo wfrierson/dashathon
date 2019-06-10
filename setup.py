cb7cc839e9bf905ef0c83a2d994ef4a0b966f6fa
Will Frierson <will.frierson@gmail.com>
2019-06-09 19:09:55 -0700

    master: Adding draft setup file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+	  name='dashathon',
+      version='0.9.0',
+      description='A dashboard to help athletes train for marathons using 20 years of historical data',
+      author='Hallie Ertman, Will Frierson, Ishna Kaul, Edwin Matthew',
+      author_email='harriet.c.ertman@gmail.com, will.frierson@gmail.com, ishnakaul28@gmail.com, edwin100@uw.edu',
+      url='https://github.com/wfrierson/dashathon',
+      packages=setuptools.find_packages(),
+)
